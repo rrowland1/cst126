7167526b1f0bf7901a334532e1a2cb3493311877
yerboygiles <43770337+yerboygiles@users.noreply.github.com>
2020-01-15 15:56:29 -0800

    Create lab2_1_15_20_theo.cpp

---
+++ lab2_1_15_20_theo.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using std::ifstream;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int loadFromFile(char** pStrs, int* numIngredients, char* fileName);
+int displayIngredients(char** pStrs, int numIngredients); 
+
+int main() {
+	char* fileName = new char[30];
+	int* numIngredients = new int;
+	cin >> *numIngredients;
+	strcpy_s(fileName,sizeof("food.txt"), "food.txt");
+	char ** pantry = new char * [360];
+
+
+	loadFromFile(pantry, numIngredients, fileName);
+	displayIngredients(pantry, *numIngredients);
+
+
+	for (int i = 0; i < *numIngredients; ++i)
+		delete[] pantry[i];
+	delete[] pantry;
+	delete[] fileName;
+	delete numIngredients;
+
+	return 0;
+}
+int loadFromFile(char** pStrs, int* numIngredients, char* fileName) {
+	int counter = 0;
+	char buffer[35];
+	ifstream fin("food.txt");
+
+	while (!fin.eof()&&(counter<*numIngredients)) {
+		fin >> buffer;
+
+		pStrs[counter] = new char[strlen(buffer) + 1];
+		strcpy_s(pStrs[counter], strlen(buffer) + 1, buffer);
+
+		counter++;
+	}
+}
+int displayIngredients(char** pStrs, int numIngredients) {
+	for (int i = 0; i < numIngredients; i++) {
+		cout << pStrs[i] << endl;
+	}
+}
